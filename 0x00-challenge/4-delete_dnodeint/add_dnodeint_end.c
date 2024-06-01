67be48114a4056e087dd729c03c78c915ff2f730
paula raafat <paularaafat27@.com>
2024-06-01 18:36:26 +0300

    fix my code

--- 0x00-challenge/4-delete_dnodeint/add_dnodeint_end.c
+++ 0x00-challenge/4-delete_dnodeint/add_dnodeint_end.c
@@ -15,18 +15,15 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
     dlistint_t *new;
     dlistint_t *l;
 
-    // Allocate memory for the new node
     new = malloc(sizeof(dlistint_t));
     if (new == NULL)
     {
         return (NULL);
     }
 
-    // Initialize the new node
     new->n = n;
     new->next = NULL;
 
-    // If the list is empty, the new node becomes the head
     if (*head == NULL)
     {
         *head = new;
@@ -34,14 +31,12 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
         return (new);
     }
 
-    // Traverse to the last node of the list
     l = *head;
     while (l->next != NULL)
     {
         l = l->next;
     }
 
-    // Link the new node at the end
     l->next = new;
     new->prev = l;
 
